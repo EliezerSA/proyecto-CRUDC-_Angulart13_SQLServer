6f1402e889d75cb98bc71afda31669f16936cb29
EliezerSA <eliezersantiago-100@outlook.com>
2024-01-12 16:27:52 -0600

    Version Inicial del Proyecto CRUD CON ASP.NET + ANGULAR CLI 13 + SQL SERVER 2019

---
+++ back_mascota/App_Start/WebApiConfig.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web.Http;
+
+namespace back_mascota
+{
+    public static class WebApiConfig
+    {
+        public static void Register(HttpConfiguration config)
+        {
+            // Configuración y servicios de Web API
+
+            // Rutas de Web API
+            config.MapHttpAttributeRoutes();
+
+            config.Routes.MapHttpRoute(
+                name: "DefaultApi",
+                routeTemplate: "api/{controller}/{id}",
+                defaults: new { id = RouteParameter.Optional }
+            );
+
+
+            GlobalConfiguration.Configuration.Formatters.JsonFormatter.MediaTypeMappings
+                .Add(new System.Net.Http.Formatting.RequestHeaderMapping("Accept",
+                "text/html",
+                StringComparison.InvariantCultureIgnoreCase,
+                true,
+                "application/json"));
+        }
+    }
+}
---
+++ back_mascota/Models/Mascota.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace back_mascota.Models
+{
+    public class Mascota
+    {
+        public int idMascota { get; set; }
+        public string nombre { get; set; }
+        public int edad { get; set; }
+        public string descripcion{ get; set; }
+
+
+        public Mascota() { }
+
+        public Mascota(int id, string Nombre, int Edad, string desc)
+        {
+            idMascota = id;
+            nombre = Nombre;
+            edad = Edad;
+            descripcion = desc;
+
+        }
+
+        public Mascota(string Nombre, int Edad, string desc)
+        {
+            nombre = Nombre;
+            edad = Edad;
+            descripcion = desc;
+
+        }
+    }
+}
